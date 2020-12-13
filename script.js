a3631ecaedb32a74713ab1dbddd393fe2d0bd67e
Enes <enes.durmus3738@gmail.com>
2020-12-13 15:27:15 +0300

    Created notes list,styles is separeted from html.

--- script.js
+++
@@ -1,7 +0,0 @@
-function play() {
-    var audio = document.getElementById("audio");
-    console.log("bastın");
-    audio.play();
-  }
-
-  document.getElementById("1").addEventListener("click", play);
