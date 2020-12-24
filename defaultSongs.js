fefb60e4c44cf2b0cd722f3d129ecdee16c7c42c
Enes <enes.durmus3738@gmail.com>
2020-12-24 22:09:15 +0300

    Fixing stopPlay, adding icons, fixing record anim.

--- defaultSongs.js
+++ defaultSongs.js
@@ -1,10 +1,11 @@
 function FillDefaultSongs() {
+    record = [];
     note0 = new Note(0,500);
     note1 = new Note(1,500);
     note2 = new Note(2,500);
     note3 = new Note(3,500);
     note4 = new Note(4,500);
-    note5 = new Note(1,500);
+    note5 = new Note(1,0);
     note6 = new Note(5,500);
     record.push(note0);
     record.push(note1);
